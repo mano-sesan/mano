77c156e16ba0fee4d4c41ee14c2d17e7ac3a4f16
Raphaël Huchet <raph@selego.co>
2024-04-04 10:21:04 +0200

    chore: up

--- app/src/Navigators.js
+++ app/src/Navigators.js
@@ -62,6 +62,7 @@ import { currentTeamState, organisationState, teamsState, userState } from './re
 import { appCurrentCacheKey, clearCache } from './services/dataManagement';
 import useResetAllCachedDataRecoilStates from './recoil/reset';
 import CGUsAcceptance from './scenes/Login/CGUsAcceptance';
+import TerritoryObservationRencontre from './scenes/Territories/TerritoryObservationRencontre';
 
 const ActionsStack = createStackNavigator();
 const ActionsNavigator = () => {
@@ -129,6 +130,8 @@ const TerritoriesNavigator = () => {
       <TerritoriesStack.Screen name="NewTerritoryForm" component={NewTerritoryForm} />
       <TerritoriesStack.Screen name="Territory" component={Territory} />
       <TerritoriesStack.Screen name="TerritoryObservation" component={TerritoryObservation} />
+      <TerritoriesStack.Screen name="TerritoryObservationRencontre" component={TerritoryObservationRencontre} />
+      <TerritoriesStack.Screen name="PersonsSearch" component={PersonsSearch} />
     </TerritoriesStack.Navigator>
   );
 };
@@ -356,10 +359,17 @@ const App = () => {
     onLogout();
   }, [isLoggedIn]);
 
+  const onNavigationStateChange = async () => {
+    if (!navigationRef.current) return;
+    const state = navigationRef.current.getRootState();
+    // console.log(JSON.stringify(state, null, 2));
+  };
+
   return (
     <ActionSheetProvider>
       <NavigationContainer
         ref={navigationRef}
+        onStateChange={onNavigationStateChange}
         onReady={() => {
           API.navigation = navigationRef;
         }}>
