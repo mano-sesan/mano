de31331afabcfb65f1e3acfac297f1e132756b62
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-04-24 11:12:04 +0200

    feat: possibilité de trier par le nombre de consultations partout dans l'espace mano web

--- dashboard/src/recoil/selectors.js
+++ dashboard/src/recoil/selectors.js
@@ -243,6 +243,8 @@ export const itemsGroupedByPersonSelector = selector({
       personsObject[consultation.person].consultations = personsObject[consultation.person].consultations || [];
       personsObject[consultation.person].consultations.push(consultation);
       personsObject[consultation.person].hasAtLeastOneConsultation = true;
+      if (!personsObject[consultation.person].numberOfConsultations) personsObject[consultation.person].numberOfConsultations = 0;
+      personsObject[consultation.person].numberOfConsultations++;
       personsObject[consultation.person].interactions.push(consultation.dueAt);
       personsObject[consultation.person].interactions.push(consultation.createdAt);
       personsObject[consultation.person].interactions.push(consultation.completedAt);
