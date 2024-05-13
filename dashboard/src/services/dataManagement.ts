93bf9aabd78036c8258a550b04cb7e6da9997d17
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-05-13 18:18:36 +0200

    disable on playwright

--- dashboard/src/services/dataManagement.ts
+++ dashboard/src/services/dataManagement.ts
@@ -3,17 +3,21 @@ import { capture } from "./sentry";
 
 export const dashboardCurrentCacheKey = "mano_last_refresh_2022_01_11";
 export const manoDB = "mano-dashboard";
-const customStore = createStore(manoDB, dashboardCurrentCacheKey);
+
+let customStore: any = null;
+if (import.meta.env.VITE_TEST_PLAYWRIGHT !== "true") {
+  customStore = createStore(manoDB, dashboardCurrentCacheKey);
+}
 
 export async function clearCache(calledFrom: string) {
-  clear(customStore);
+  if (customStore) clear(customStore);
   window.localStorage?.clear();
   window.sessionStorage?.clear();
 }
 
 export async function setCacheItem(key: string, value: any) {
   try {
-    await set(key, value, customStore);
+    if (customStore) await set(key, value, customStore);
   } catch (error) {
     capture(error, { tags: { key } });
   }
@@ -21,6 +25,7 @@ export async function setCacheItem(key: string, value: any) {
 
 export async function getCacheItem(key: string) {
   try {
+    if (customStore === null) return null;
     const data = await get(key, customStore);
     return data;
   } catch (error) {
