eb19ea4b1894217013795e3435006b4a004c3935
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-05-16 18:21:04 +0200

    fixed !

--- dashboard/src/services/dataManagement.ts
+++ dashboard/src/services/dataManagement.ts
@@ -1,5 +1,7 @@
 import { type UseStore, set, get, createStore } from "idb-keyval";
 import { capture } from "./sentry";
+import type { UUIDV4 } from "../types/uuid";
+import { decryptDBItem } from "./api";
 
 export const dashboardCurrentCacheKey = "mano_last_refresh_2022_01_11";
 export const manoDB = "mano-dashboard";
@@ -23,6 +25,13 @@ async function deleteDB() {
     DBDeleteRequest.onerror = (event) => {
       capture(event); // just to monitor, rejecting would block the process, maybe we can remove this capture
       resolve(false);
+<<<<<<< HEAD
+=======
+    };
+    DBDeleteRequest.onblocked = (event) => {
+      capture(event); // just to monitor, rejecting would block the process, maybe we can remove this capture
+      resolve(false);
+>>>>>>> 90ba6ed9 (fixed !)
     };
     DBDeleteRequest.onblocked = () => {
       resolve(false);
@@ -63,3 +72,32 @@ export async function getCacheItemDefaultValue(key: string, defaultValue: any) {
   const storedValue = await getCacheItem(key);
   return storedValue || defaultValue;
 }
+
+
+interface EncryptedItem {
+  _id: UUIDV4;
+  organisation: UUIDV4;
+  encrypted: string;
+  encryptedEntityKey: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+
+  [key: string]: any;
+}
+
+export let cachedCollectionsNames = {
+  person: "person",
+  action: "action",
+  // TODO: Add more collections here
+} as const;
+
+type CollectionsNames = keyof typeof cachedCollectionsNames;
+
+export async function getDecryptedCache(key: CollectionsNames) {
+  const cryptedCache = await getCacheItemDefaultValue(key, []);
+  const decryptedCache = await Promise.all(cryptedCache.map((item: EncryptedItem) => decryptDBItem(item, { path: `${key}-cache` })));
+  return decryptedCache;
+}
+
