bac00a3db0c58f58270660bd7b8f69fb6bfde786
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-05-28 18:00:43 +0200

    Update dashboard/src/scenes/organisation/view.tsx
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- dashboard/src/scenes/organisation/view.tsx
+++ dashboard/src/scenes/organisation/view.tsx
@@ -131,7 +131,7 @@ const View = () => {
           <button type="button" onClick={() => setTab("encryption")} className="tw-font-bold tw-text-stone-800 tw-underline">
             Cliquez ici
           </button>{" "}
-          ou sur le bouton "Chiffrement" pour la modifier.
+          ou sur le bouton «&nbsp;Chiffrement&nbsp;» pour la modifier.
         </div>
       )}
       <div className="tw-flex tw-flex-1 tw-overflow-hidden">
