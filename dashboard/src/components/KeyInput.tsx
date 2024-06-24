dc4031dcf26bb1056384b3194b7c0574157ce91d
Raphaël Huchet <raph@selego.co>
2024-06-24 12:18:04 +0200

    fix: autofocus sur le sélecteur de clé

--- dashboard/src/components/KeyInput.tsx
+++ dashboard/src/components/KeyInput.tsx
@@ -25,6 +25,8 @@ const KeyInput = ({
       setValue(DEFAULT_ORGANISATION_KEY);
       inputRef.current.innerText = "•".repeat(DEFAULT_ORGANISATION_KEY.length);
     }
+    // Focus sur le champ
+    inputRef.current.focus();
   }, []);
 
   const isAndroid = () => {
@@ -41,6 +43,8 @@ const KeyInput = ({
       <div
         id={id}
         contentEditable
+        // L'auto-focus ne semble pas fonctionner ici, donc on utilise un useEffect pour forcer le focus.
+        autoFocus
         ref={inputRef}
         onPaste={(e) => {
           // Pas de copier/coller
