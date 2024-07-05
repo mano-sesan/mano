d176b99592366926545c425940d5ca2a020f9cf4
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-07-05 15:07:39 +0200

    Update dashboard/src/components/DeleteButtonAndConfirmModal.tsx
    Co-authored-by: Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>

--- dashboard/src/components/DeleteButtonAndConfirmModal.tsx
+++ dashboard/src/components/DeleteButtonAndConfirmModal.tsx
@@ -50,7 +50,7 @@ const DeleteButtonAndConfirmModal = ({
       <ModalContainer open={open} onClose={() => setOpen(false)} size="3xl">
         <ModalHeader>
           <div className="tw-px-4">
-            <div className="tw-block tw-text-center tw-text-xl tw-text-red-500">{title}</div>
+            <h2 className="tw-block tw-text-center tw-text-xl tw-text-red-500">{title}</h2>
           </div>
         </ModalHeader>
         <ModalBody className="tw-py-4">
