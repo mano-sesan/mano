73875077e72299949d94c1ec2db1a7f1909f16c4
Raphaël Huchet <raph@selego.co>
2024-07-15 15:08:59 +0200

    up

--- dashboard/src/scenes/organisation/Errors.jsx
+++ dashboard/src/scenes/organisation/Errors.jsx
@@ -1,7 +1,7 @@
 import API, { tryFetchExpectOk } from "../../services/api";
 import { decrypt, derivedMasterKey, encryptItem, getHashedOrgEncryptionKey } from "../../services/encryption";
 import { useRecoilValue } from "recoil";
-import { organisationState } from "../../recoil/auth";
+import { organisationState, userState } from "../../recoil/auth";
 import { useEffect, useState } from "react";
 import Loading from "../../components/loading";
 import Table from "../../components/table";
@@ -19,41 +19,60 @@ const getErroredDecryption = async (item) => {
   return null;
 };
 
-async function fetchErroredPersons(organisationId) {
+async function fetchErrored(organisationId, path) {
   const query = {
     organisation: organisationId,
     limit: String(10000),
-    withDeleted: true,
   };
   let page = 0;
   let finished = false;
   const erroredPersons = [];
   while (!finished) {
     const [error, res] = await tryFetchExpectOk(async () => {
-      return API.get({ path: "/person", query: { ...query, page: String(page) } });
+      return API.get({ path, query: { ...query, page: String(page) } });
     });
     if (error) {
-      toast.error("Erreur lors de la récupération des personnes en erreur, vraiment pas de chance");
+      toast.error("Erreur lors de la récupération des données en erreur, pas de chance");
       return [];
     }
     if (!res.hasMore) finished = true;
     const decryptedData = (await Promise.all(res.data.map((p) => getErroredDecryption(p)))).filter((e) => e);
-    erroredPersons.push(...decryptedData.map((p) => ({ _id: p._id, type: "person", data: p })));
+    erroredPersons.push(...decryptedData.map((p) => ({ _id: p._id, type: path, data: p })));
   }
   return erroredPersons;
 }
 
 export default function Errors() {
+  const user = useRecoilValue(userState);
+  const { refresh } = useDataLoader();
   const [open, setOpen] = useState(false);
   const [item, setItem] = useState(undefined);
   const [data, setData] = useState(undefined);
   const organisation = useRecoilValue(organisationState);
 
   useEffect(() => {
-    fetchErroredPersons(organisation._id).then((data) => {
-      setData(data);
-    });
-  }, [organisation._id]);
+    (async () => {
+      let res = [];
+      res.push(...(await fetchErrored(organisation._id, "person")));
+      res.push(...(await fetchErrored(organisation._id, "group")));
+      res.push(...(await fetchErrored(organisation._id, "report")));
+      res.push(...(await fetchErrored(organisation._id, "passage")));
+      res.push(...(await fetchErrored(organisation._id, "rencontre")));
+      res.push(...(await fetchErrored(organisation._id, "action")));
+      res.push(...(await fetchErrored(organisation._id, "territory")));
+      res.push(...(await fetchErrored(organisation._id, "place")));
+      res.push(...(await fetchErrored(organisation._id, "relPersonPlace")));
+      res.push(...(await fetchErrored(organisation._id, "territory-observation")));
+      res.push(...(await fetchErrored(organisation._id, "comment")));
+      if (user.healthcareProfessional) {
+        res.push(...(await fetchErrored(organisation._id, "consultation")));
+        res.push(...(await fetchErrored(organisation._id, "treatment")));
+        res.push(...(await fetchErrored(organisation._id, "medical-file")));
+      }
+
+      setData(res);
+    })();
+  }, [organisation._id, user.healthcareProfessional]);
 
   if (!data)
     return (
@@ -65,8 +84,6 @@ export default function Errors() {
         </div>
       </>
     );
-
-  console.log(data);
 
   return (
     <div>
@@ -102,11 +119,66 @@ export default function Errors() {
               render: (item) => (
                 <button
                   className="button-destructive"
-                  onClick={() => {
-                    if (!confirm("Êtes-vous sûr de vouloir supprimer DEFINITIVEMENT cette donnée ?")) return;
+                  onClick={async () => {
+                    if (!confirm("Êtes-vous sûr de vouloir supprimer cette donnée ?")) return;
                     if (item.type === "person") {
-                      // TODO
+                      await API.delete({
+                        path: `/person/${item.data._id}`,
+                        body: {
+                          actionsToTransfer: [],
+                          commentsToTransfer: [],
+                          actionIdsToDelete: [],
+                          commentIdsToDelete: [],
+                          passageIdsToDelete: [],
+                          rencontreIdsToDelete: [],
+                          consultationIdsToDelete: [],
+                          treatmentIdsToDelete: [],
+                          medicalFileIdsToDelete: [],
+                          relsPersonPlaceIdsToDelete: [],
+                        },
+                      });
                     }
+                    if (item.type === "group") {
+                      await API.delete({ path: `/group/${item.data._id}` });
+                    }
+                    if (item.type === "report") {
+                      await API.delete({ path: `/report/${item.data._id}` });
+                    }
+                    if (item.type === "passage") {
+                      await API.delete({ path: `/passage/${item.data._id}` });
+                    }
+                    if (item.type === "rencontre") {
+                      await API.delete({ path: `/rencontre/${item.data._id}` });
+                    }
+                    if (item.type === "action") {
+                      await API.delete({ path: `/action/${item.data._id}` });
+                    }
+                    if (item.type === "territory") {
+                      await API.delete({ path: `/territory/${item.data._id}` });
+                    }
+                    if (item.type === "place") {
+                      await API.delete({ path: `/place/${item.data._id}` });
+                    }
+                    if (item.type === "relPersonPlace") {
+                      await API.delete({ path: `/relPersonPlace/${item.data._id}` });
+                    }
+                    if (item.type === "territory-observation") {
+                      await API.delete({ path: `/territory-observation/${item.data._id}` });
+                    }
+                    if (item.type === "comment") {
+                      await API.delete({ path: `/comment/${item.data._id}` });
+                    }
+                    if (user.healthcareProfessional && item.type === "consultation") {
+                      await API.delete({ path: `/consultation/${item.data._id}` });
+                    }
+                    if (user.healthcareProfessional && item.type === "treatment") {
+                      await API.delete({ path: `/treatment/${item.data._id}` });
+                    }
+                    if (user.healthcareProfessional && item.type === "medical-file") {
+                      await API.delete({ path: `/medical-file/${item.data._id}` });
+                    }
+                    await refresh();
+                    toast.success("L'élément a été supprimé !");
                   }}
                 >
                   Supprimer
@@ -122,6 +194,7 @@ export default function Errors() {
 }
 
 function ModalRepair({ open, setOpen, item }) {
+  const user = useRecoilValue(userState);
   const [key, setKey] = useState("");
   const { refresh } = useDataLoader();
 
@@ -144,8 +217,48 @@ function ModalRepair({ open, setOpen, item }) {
     if (item.type === "person") {
       await API.put({ path: `/person/${itemData._id}`, body: encryptedItem });
     }
+    if (item.type === "group") {
+      await API.put({ path: `/group/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "report") {
+      await API.put({ path: `/report/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "passage") {
+      await API.put({ path: `/passage/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "rencontre") {
+      await API.put({ path: `/rencontre/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "action") {
+      await API.put({ path: `/action/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "territory") {
+      await API.put({ path: `/territory/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "place") {
+      await API.put({ path: `/place/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "relPersonPlace") {
+      await API.put({ path: `/relPersonPlace/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "territory-observation") {
+      await API.put({ path: `/territory-observation/${itemData._id}`, body: encryptedItem });
+    }
+    if (item.type === "comment") {
+      await API.put({ path: `/comment/${itemData._id}`, body: encryptedItem });
+    }
+    if (user.healthcareProfessional && item.type === "consultation") {
+      await API.put({ path: `/consultation/${itemData._id}`, body: encryptedItem });
+    }
+    if (user.healthcareProfessional && item.type === "treatment") {
+      await API.put({ path: `/treatment/${itemData._id}`, body: encryptedItem });
+    }
+    if (user.healthcareProfessional && item.type === "medical-file") {
+      await API.put({ path: `/medical-file/${itemData._id}`, body: encryptedItem });
+    }
     await refresh();
-    toast.success("L'élément a été remis à jour !");
+    toast.success("L'élément a été réparé !");
+    setOpen(false);
   }
 
   if (!item) return null;
