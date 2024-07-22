d6cea010b4ca4b9832a3e63338d926edeac9caf2
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-07-22 15:22:40 +0200

    fix: meilleure performance dans le dataloader en évitant de tout recalculer lorsqu'il y a un changement dans organisation/teams/user

--- dashboard/src/components/drawer.jsx
+++ dashboard/src/components/drawer.jsx
@@ -5,6 +5,7 @@ import OpenNewWindowIcon from "./OpenNewWindowIcon";
 import SessionCountDownLimiter from "./SessionCountDownLimiter";
 import useMinimumWidth from "../services/useMinimumWidth";
 import { deploymentShortCommitSHAState } from "../recoil/version";
+import AddPersons from "./AddPersons";
 
 export const showDrawerState = atom({
   key: "showDrawerState",
@@ -154,6 +155,14 @@ const Drawer = () => {
                   )}
                 </NavLink>
               </li>
+              {process.env.NODE_ENV === "development" && (
+                <>
+                  <hr />
+                  <li>
+                    <AddPersons />
+                  </li>
+                </>
+              )}
             </>
           )}
         </div>
