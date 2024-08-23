ae0cb7d2645ec5b3385eb5ae72392786ad7f8504
Raphaël Huchet <raph@selego.co>
2024-08-23 16:52:10 +0200

    fix: réparation du sélecteur d'utilisateur qui était tout pété bizarrement

--- dashboard/src/components/SelectUser.tsx
+++ dashboard/src/components/SelectUser.tsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
 import { useRecoilValue } from "recoil";
 import { usersState } from "../recoil/auth";
 import SelectCustom from "./SelectCustom";
 
 const SelectUser = ({ value, onChange, ...props }) => {
-  const [search, setSearch] = useState("");
   const allUsers = useRecoilValue(usersState);
-
-  const users = search?.length
-    ? allUsers
-        .filter((i) => i.name)
-        .filter((p) => {
-          if (p.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) return true;
-          if ((p.email || "").toLocaleLowerCase().includes(search.toLocaleLowerCase())) return true;
-          return false;
-        })
-    : allUsers.filter((i) => i.name);
 
   return (
     <SelectCustom
-      options={users}
+      options={allUsers.filter((u) => u.name)}
       name="user"
-      inputValue={search}
-      onInputChange={setSearch}
-      isSearchable
-      onChange={(v) => {
-        setSearch(v?.name);
-        onChange(v?._id);
-      }}
-      value={users.filter((i) => i._id === value)[0]}
+      isClearable={false}
+      onChange={(v) => onChange(v?._id)}
+      value={allUsers.filter((i) => i._id === value)[0]}
       placeholder={" -- Choisir un utilisateur -- "}
       getOptionValue={(i) => i._id}
       getOptionLabel={(i) => i?.name}
