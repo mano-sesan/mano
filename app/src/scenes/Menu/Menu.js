7bef1fb4cd820f08adfdf853a2ea143456fb6ad0
Raphaël Huchet <raph@selego.co>
2024-09-03 17:15:58 +0200

    chore: suppression du formulaire donnez votre avis sur l'appli mano

--- app/src/scenes/Menu/Menu.js
+++ app/src/scenes/Menu/Menu.js
@@ -6,7 +6,7 @@ import Row from '../../components/Row';
 import Spacer from '../../components/Spacer';
 import API from '../../services/api';
 import ScrollContainer from '../../components/ScrollContainer';
-import { FRAMAFORM_MANO, MANO_DOWNLOAD_URL, MANO_TEST_ORGANISATION_ID } from '../../config';
+import { MANO_DOWNLOAD_URL, MANO_TEST_ORGANISATION_ID } from '../../config';
 import { useRecoilValue } from 'recoil';
 import { currentTeamState, organisationState } from '../../recoil/auth';
 import { capture } from '../../services/sentry';
@@ -39,7 +39,6 @@ const Menu = ({ navigation }) => {
         <Row withNextButton caption={`Changer d'équipe (actuellement ${currentTeam?.name})`} onPress={() => navigation.navigate('ChangeTeam')} />
         <Row withNextButton caption="Changer le mot de passe" onPress={() => navigation.navigate('ChangePassword')} />
         <Spacer height={30} />
-        <Row withNextButton caption="Donnez votre avis sur l'app" onPress={() => Linking.openURL(FRAMAFORM_MANO)} />
         <Row withNextButton caption="Télécharger Mano" onPress={() => Linking.openURL(MANO_DOWNLOAD_URL)} />
         <Spacer height={30} />
         <Row withNextButton caption="Charte des utilisateurs" onPress={() => navigation.navigate('Charte')} />
