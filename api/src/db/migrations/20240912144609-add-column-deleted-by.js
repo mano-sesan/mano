77d2455b82a774d3c6829d57d25a949f72b8f948
Raphaël Huchet <raph@selego.co>
2024-09-13 13:40:52 +0200

    fix: oops

--- api/src/db/migrations/20240912144609-add-column-deleted-by.js
+++ api/src/db/migrations/20240912144609-add-column-deleted-by.js
@@ -5,55 +5,59 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Action" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Action" ADD CONSTRAINT "Action_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Action"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Action" ADD CONSTRAINT "Action_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Comment" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Comment" ADD CONSTRAINT "Comment_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Comment"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Comment" ADD CONSTRAINT "Comment_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Consultation" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Consultation" ADD CONSTRAINT "Consultation_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Consultation"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Consultation" ADD CONSTRAINT "Consultation_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Group" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Group" ADD CONSTRAINT "Group_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Group"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Group" ADD CONSTRAINT "Group_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."MedicalFile" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."MedicalFile" ADD CONSTRAINT "MedicalFile_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."MedicalFile"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."MedicalFile" ADD CONSTRAINT "MedicalFile_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Passage" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Passage" ADD CONSTRAINT "Passage_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Passage"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Passage" ADD CONSTRAINT "Passage_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Person" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Person" ADD CONSTRAINT "Person_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Person"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Person" ADD CONSTRAINT "Person_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Place" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Place" ADD CONSTRAINT "Place_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Place"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Place" ADD CONSTRAINT "Place_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."RelPersonPlace" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."RelPersonPlace" ADD CONSTRAINT "RelPersonPlace_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."RelPersonPlace"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."RelPersonPlace" ADD CONSTRAINT "RelPersonPlace_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Rencontre" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Rencontre" ADD CONSTRAINT "Rencontre_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Rencontre"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Rencontre" ADD CONSTRAINT "Rencontre_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
+    );
+    await queryInterface.sequelize.query(`ALTER TABLE "mano"."Report" ADD COLUMN "deletedBy" UUID;`);
+    await queryInterface.sequelize.query(
+      `ALTER TABLE "mano"."Report" ADD CONSTRAINT "Report_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Territory" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Territory" ADD CONSTRAINT "Territory_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Territory"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Territory" ADD CONSTRAINT "Territory_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."TerritoryObservation" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."TerritoryObservation" ADD CONSTRAINT "TerritoryObservation_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."TerritoryObservation"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."TerritoryObservation" ADD CONSTRAINT "TerritoryObservation_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
     await queryInterface.sequelize.query(`ALTER TABLE "mano"."Treatment" ADD COLUMN "deletedBy" UUID;`);
     await queryInterface.sequelize.query(
-      `ALTER TABLE "mano"."Treatment" ADD CONSTRAINT "Treatment_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."Treatment"("_id") ON DELETE SET NULL;`
+      `ALTER TABLE "mano"."Treatment" ADD CONSTRAINT "Treatment_deletedBy_fkey" FOREIGN KEY ("deletedBy") REFERENCES "mano"."User"("_id") ON DELETE SET NULL;`
     );
   },
 
