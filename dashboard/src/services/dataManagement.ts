3c31a4b996365874cb62923c0f21caf20c0341b3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-10-21 11:20:44 +0200

    new cache key

--- dashboard/src/services/dataManagement.ts
+++ dashboard/src/services/dataManagement.ts
@@ -1,7 +1,7 @@
 import { type UseStore, set, get, createStore, keys, delMany, clear } from "idb-keyval";
 import { capture } from "./sentry";
 
-export const dashboardCurrentCacheKey = "mano_last_refresh_2024_06_11";
+export const dashboardCurrentCacheKey = "mano_last_refresh_2024_10_21";
 const legacyStoreName = "mano_last_refresh_2022_01_11";
 const legacyManoDB = "mano-dashboard";
 const manoDB = "mano";
