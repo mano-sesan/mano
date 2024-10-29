1da039481ba6caaf86bc1b92f7af3e0b8671df14
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-10-29 12:09:47 +0100

    feat(app): on montre les actions récurrentes dans le dossier des personnes suivies (#689)

---
+++ app/src/icons/RepeatIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+export default function RepeatIcon({ className }) {
+  return (
+    <Svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4 text-main">
+      <Path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        // eslint-disable-next-line max-len
+        d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+      />
+    </Svg>
+  );
+}
