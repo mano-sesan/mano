4107651765d9079ba59434161da16537ade13aab
Raphaël Huchet <raph@selego.co>
2024-10-29 10:04:24 +0100

    fix: on peut faire des tranfert d'équipe de plus de 50mb (il y avait une sécurité sur toutes les routes qui s'appliquait à celle là aussi)

--- api/src/index.js
+++ api/src/index.js
@@ -64,7 +64,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   // if you change these values, you need to change them also in
   // https://github.com/mano-sesan/mano/blob/main/.server/vhost/api-mano.sesan.fr#L1
-  const limitedSize = req.path.startsWith("/encrypt") ? "350mb" : "50mb";
+  const limitedSize = req.path.startsWith("/encrypt") || req.path.startsWith("/transfer-team") ? "350mb" : "50mb";
   express.json({ limit: limitedSize })(req, res, next);
 });
 app.use(helmet());
