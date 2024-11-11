08a0bf175db3483b4afbf1a1935f8e6f8fe6f6bb
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2024-11-11 17:22:49 +0100

    chore: tag type for sentry error decrypt item

--- dashboard/src/services/encryption.js
+++ dashboard/src/services/encryption.js
@@ -224,7 +224,7 @@ export const decryptItem = async (item, { decryptDeleted = false, type = "" } = 
     capture(`ERROR DECRYPTING ${type || "ITEM"} ${item?._id} : ${errorDecrypt}`, {
       fingerprint: [`{{ default }}`, item._id],
       extra: { message: "ERROR DECRYPTING ITEM", item, type },
-      tags: { _id: item._id },
+      tags: { _id: item._id, type },
     });
     return null;
   }
