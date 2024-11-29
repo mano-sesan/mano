a2c3e5e6a86ca3c28ff764cd216a69b09306a0df
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-11-29 08:56:16 +0100

    fix(app): afficher "tout le monde (oui et non)" polutôt que vide dans le filtre de sortie de file active (#754)

--- app/src/components/Selects/OutOfActiveListSelect.js
+++ app/src/components/Selects/OutOfActiveListSelect.js
@@ -5,8 +5,29 @@ import SelectLabelled from './SelectLabelled';
 
 const OutOfActiveListSelect = ({ value = '', onSelect, editable }) => {
   const personFields = useRecoilValue(personFieldsSelector);
-  const options = ['', ...personFields.find((f) => f.name === 'outOfActiveList').options];
-  return <SelectLabelled label="Sortie de file active" values={options} value={value} onSelect={onSelect} editable={editable} />;
+  const options = [
+    {
+      _id: 'all',
+      name: 'Tout le monde (oui et non)',
+    },
+    ...personFields.find((f) => f.name === 'outOfActiveList').options.map((o) => ({ _id: o, name: o })),
+  ];
+  return (
+    <SelectLabelled
+      mappedIdsToLabels={options}
+      label="Sortie de file active"
+      values={options.map((o) => o._id)}
+      value={value}
+      onSelect={(value) => {
+        if (value === 'all') {
+          onSelect('');
+        } else {
+          onSelect(value);
+        }
+      }}
+      editable={editable}
+    />
+  );
 };
 
 export default OutOfActiveListSelect;
