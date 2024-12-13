8608a20af0c7ab32fc592809bdb3cabd9dea663d
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-12-13 09:18:49 +0100

    fix: nouvelle charte des utilisateurs (#796)

--- dashboard/src/scenes/auth/charte.jsx
+++ dashboard/src/scenes/auth/charte.jsx
@@ -26,121 +26,113 @@ const Charte = () => {
       <small className="tw-block tw-text-center tw-font-medium tw-italic">
         Veuillez lire et accepter la Charte d'Utilisation de Mano avant de continuer
       </small>
-      <main className="[&_b]:tw-font-weight-normal tw-w-full tw-max-w-prose tw-px-5 [&_b]:tw-mt-20 [&_b]:tw-block [&_ol_>_li]:tw-mb-8 [&_ol_>_li]:tw-mt-20 [&_ol_>_li]:tw-font-bold [&_span]:tw-mb-8 [&_span]:tw-block [&_ul>li]:tw-mb-4">
-        <ol>
-          <li>Présentation du dispositif et propriété des données</li>
-          <p>
-            MANO est un outil destiné aux maraudes, équipes mobiles et professionnel·le·s intervenant sur des lieux d’accueil. Il comprend une
-            application accessible en rue et une interface web.
-            <br />
-            <br />
-            Les données (personnes suivies, actions, territoires, statistiques, comptes-rendus) sont stockées par MANO sur des serveurs protégés et
-            agréés “données de santé” et sont la propriété de chaque structure utilisatrice. Le présent document est une charte encadrant la création,
-            le stockage et l'utilisation de ces données. Cette charte a été créée avec les utilisateurs et continuera d'évoluer au fur et à mesure des
-            retours des utilisateurs.
-            <br />
-            <br />
-            MANO est un outil gratuit.
-          </p>
-          <li>Objectifs et approche terrain</li>
-          <ul>
-            <li>
-              MANO a pour finalité d’outiller ses utilisateurs dans l’accompagnement de leurs publics et ainsi faciliter l’accompagnement de ces
-              personnes.
-            </li>
-            <li>
-              L’équipe MANO s’engage à penser tout développement en cherchant à être au plus proche de l’éthique professionnelle inhérente au travail
-              médico-social des utilisateurs.
-            </li>
-            <li>
-              L’équipe MANO s’engage à organiser un écosystème&nbsp;: des réunions inter-services utilisant l’outil sont organisées une fois par mois.
-              Ces points sont l'occasion d’échanger sur les pratiques et expériences avec MANO et de remonter les besoins du terrain.
-            </li>
-            <li>
-              L’équipe MANO s’engage à prendre en compte les retours du terrain et à inclure les professionnel·le·s dans le processus de développement
-              de l'outil.
-            </li>
-          </ul>
-          <li>Installation et prise en main</li>
-          <ul>
-            <li>L’équipe MANO s’engage à présenter l’outil et à former les équipes utilisatrices.</li>
-            <li>L’équipe MANO s’engage à accompagner les équipes dans l'utilisation de MANO sur le terrain.</li>
-            <li>L’équipe MANO s’engage à fournir une assistance téléphonique.</li>
-          </ul>
-          <li>Évolution de l'outil</li>
-          <p>
-            MANO évolue en continu. Les modifications apportées sont faites sur la base des retours des utilisateurs. A cet effet, ils et elles
-            peuvent remonter des besoins ou des propositions d'évolution au travers des questionnaires disponibles dans l'onglet "Profil". L'équipe
-            MANO s'engage à en prendre connaissance sous 3 jours ouvrés. Les équipes peuvent également regrouper leurs retours et les soumettre lors
-            des réunions mensuelles inter-services.
-          </p>
-          <li>Aspects techniques</li>
-          <ul>
-            <li>Les utilisateurs peuvent signaler tout dysfonctionnement ou soumettre toute idée via le formulaire de satisfaction dans l'outil.</li>
-            <li>L’équipe MANO s’engage à effectuer la maintenance du produit.</li>
-          </ul>
-          <li>Aspects juridiques et protection des données</li>
-          <ul>
-            <li>Les administrateurs des équipes ont la possibilité d'écraser l’ensemble de leurs données à tout moment.</li>
-            <li>L’équipe MANO s’engage à chiffrer les données côté client et les sauvegardes.</li>
-            <li>
-              L’équipe MANO s’engage à limiter les accès aux informations. Chaque structure accède à ses données, sans pouvoir accéder aux données des
-              autres structures.
-            </li>
-            <li>
-              L’homologation du service par le ministère de la santé est initiée et couvrira une période intermédiaire jusqu'au développement du
-              service sous sa forme définitive.
-            </li>
-            <li>
-              L’équipe MANO ne peut accéder aux données des équipes et des personnes suivies. Elle peut cependant accéder aux statistiques globales
-              d’utilisation de l’outil ne contenant aucune information personnelle concernant les personnes suivies par les structures.
-            </li>
-            <li>
-              L’équipe MANO s’engage à stocker les données sur des serveurs agréés données de santé et à maintenir un niveau de sécurité optimal afin
-              de prévenir toute fuite de données pouvant porter préjudice aux personnes suivies et aux utilisateurs.
-            </li>
-          </ul>
-          <li>Engagement des utilisateurs</li>
-          <span>Tout utilisateur de MANO s'engage à protéger les données des personnes suivies&nbsp;:</span>
-          <ul>
-            <li>
-              Toute personne suivie devra être informée et donner son accord oral pour figurer dans MANO, après une brève présentation de l’outil. Il
-              est cependant possible de déroger à cette règle, notamment si la personne présente un danger pour autrui ou pour elle-même, et nécessite
-              donc une prise en charge particulière par les équipes, dont la traçabilité apportera un bénéfice dans l’accompagnement.
-            </li>
-            <li>
-              Vigilance lors de la saisie d'informations concernant les personnes suivies&nbsp;: les professionnel·le·s ne saisissent que le strict
-              nécessaire à l'accompagnement (en anonymisant notamment au maximum les dossiers). Il est cependant possible d’utiliser l’identité de la
-              personne, si elle est nécessaire à l’accompagnement.
-            </li>
-            <li>Les notions de secret professionnel et de secret professionnel partagé s'appliquent à l'utilisation de MANO.</li>
-            <li>Les identifiants et mots de passe sont personnels. Il est formellement interdit de les partager.</li>
-            <li>
-              Il est formellement interdit de partager les données d'une personne suivie sans son consentement avec des individus n'appartenant pas à
-              son équipe ou n’ayant aucun lien avec son suivi médico-social.
-            </li>
-            <li>
-              Création de compte et clôture de compte&nbsp;: les personnes qui ont un rôle d'administrateur s'engagent à maintenir à jour les accès à
-              MANO. En cas de départ d'un membre de l'équipe, son compte doit être désactivé le jour même. De même, seul·e·s les professionnel·le·s en
-              contact avec les personnes suivies ont le droit d'avoir accès à leurs données personnelles.
-            </li>
-            <li>
-              La duplication des données pour un autre usage que celui prévu par l'équipe, dans le strict cadre de sa mission est interdite. L'accès
-              au service se fait dans un contexte professionnel et non personnel. Ainsi, l’accès à l’interface web se fait exclusivement depuis un
-              poste de travail professionnel et l’accès à l’application se fait sur un téléphone professionnel. Des ordinateurs ou téléphones
-              personnels ne doivent pas être utilisés pour accéder au service.
-            </li>
-            <li>Falsification des données&nbsp;: tout utilisateur de MANO s'engage à la véracité des données qu'il renseigne.</li>
-            <li>
-              Les équipes utilisatrices s’assurent d’utiliser des ordinateurs, smartphones, tablettes, protégées par des mots de passe, ainsi que par
-              un antivirus, afin d’éviter tout risque de violation de données.
-            </li>
-          </ul>
+      <main className="[&_b]:tw-font-weight-normal tw-w-full tw-max-w-prose tw-px-5">
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Présentation du dispositif et propriété des données</h2>
+        <p>
+          MANO est un outil de suivi médico-social destiné aux professionnels sociaux et médico-sociaux intervenant auprès de publics en situation de
+          grande précarité. Il comprend une application accessible sur le terrain et une interface web. Les données des personnes accompagnées,
+          enregistrées par une organisation, sont conservées sur des serveurs sécurisés, agréés pour les données de santé. Seule la structure ayant
+          saisi ces données peut y accéder.
+          <br />
+          Ce document est une charte qui régit la création, le stockage et l'utilisation de ces données. Élaborée avec les utilisateurs, elle sera
+          mise à jour en fonction de leurs retours. MANO est un outil gratuit.
+        </p>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Objectifs et approche terrain</h2>
+        <p>
+          MANO a pour finalité d’outiller ses utilisateurs dans l’accompagnement de leurs publics et de faciliter l’activité quotidienne ainsi que le
+          suivi des actions engagées avec les usagers.
+        </p>
+        <p>
+          L’équipe MANO s’engage à concevoir tout développement en restant au plus proche de l’éthique professionnelle inhérente au travail social et
+          médico-social des utilisateurs.
+        </p>
+        <p>
+          L’équipe MANO s’engage à organiser un écosystème collaboratif : des réunions interservices utilisant l’outil sont organisées au minimum tous
+          les trois mois. Ces rencontres sont l'occasion d’échanger sur les pratiques et expériences liées à MANO, et de remonter les besoins
+          identifiés sur le terrain.
+        </p>
+        <p>
+          L’équipe MANO s’engage à prendre en compte les retours du terrain et à inclure les professionnels dans le processus de développement de
+          l'outil.
+        </p>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Installation et prise en main</h2>
+        <p>L’équipe MANO s’engage à :</p>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>Présenter l'outil et former les équipes utilisatrices.</li>
+          <li>Accompagner les équipes dans l'utilisation de MANO sur le terrain.</li>
+          <li>Fournir une assistance téléphonique.</li>
         </ol>
-        <b>
-          Tout manquement à ces engagements pourra conduire MANO à retirer l'utilisation de l'outil à un·e professionnel·le, une équipe ou une
-          organisation.
-        </b>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Évolution de l'outil</h2>
+        <p>
+          MANO évolue en continu. Les modifications apportées se basent sur les retours des utilisateurs. Pour ce faire, les utilisateurs doivent
+          remonter leurs besoins ou propositions d'évolution à la personne responsable de leur accompagnement à l’utilisation de MANO.
+        </p>
+        <p>Les équipes peuvent également regrouper leurs retours et les soumettre lors des réunions interservices.</p>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Aspects juridiques et protection des données</h2>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>Les administrateurs des équipes ont la possibilité d'effacer l'ensemble de leurs données à tout moment.</li>
+          <li>L'équipe MANO s'engage à chiffrer les données et leurs sauvegardes côté client.</li>
+          <li>
+            L'accès aux informations est limité : chaque structure accède uniquement à ses propres données, sans pouvoir consulter celles des autres
+            structures.
+          </li>
+          <li>
+            L'équipe MANO ne peut accéder aux données des équipes et des personnes suivies. Cependant, elle peut consulter les statistiques globales
+            d'utilisation de l'outil, dépourvues d'informations personnelles.
+          </li>
+          <li>
+            Les données sont stockées sur des serveurs agréés pour les données de santé, avec un niveau de sécurité optimal pour prévenir toute fuite
+            de données préjudiciable aux personnes suivies ou aux utilisateurs.
+          </li>
+        </ol>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Engagement des utilisateurs</h2>
+        <p>Tout utilisateur de MANO s'engage à protéger les données des personnes suivies&nbsp;:</p>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>
+            Toute personne suivie devra être informée et donner au minimum un accord oral pour figurer dans MANO, après une brève présentation de
+            l'outil et de ses finalités. Toutefois, une dérogation est possible, notamment si la personne présente un danger pour autrui ou pour
+            elle-même, nécessitant une prise en charge particulière par les équipes.
+          </li>
+          <li>
+            Les utilisateurs s'engagent à ne saisir que les informations strictement nécessaires à l'accompagnement, en anonymisant autant que
+            possible les dossiers. L'identité réelle peut toutefois être utilisée si elle est nécessaire à l'accompagnement.
+          </li>
+          <li>Les notions de secret professionnel et de secret professionnel partagé s'appliquent à l'utilisation de MANO.</li>
+          <li>Les identifiants et mots de passe sont strictement personnels et ne doivent pas être partagés.</li>
+          <li>
+            Il est interdit de partager les données d'une personne suivie sans son consentement à des individus extérieurs à l'équipe ou sans lien
+            avec son suivi médico-social.
+          </li>
+          <li>
+            Les administrateurs doivent mettre à jour les accès à MANO. En cas de départ d'un membre de l'équipe, son compte doit être supprimé le
+            jour même
+          </li>
+        </ol>
+        <p>Lors de la création d'un nouveau compte&nbsp;:</p>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>
+            Les administrateurs doivent solliciter l'équipe MANO pour assurer une formation des nouveaux arrivants à l'outil. Cela permet d'éviter les
+            mésusages et de promouvoir les bonnes pratiques numériques.
+          </li>
+        </ol>
+        <p>L'accès au service se fait exclusivement dans un cadre professionnel&nbsp;:</p>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>L'interface web doit être consultée sur un poste de travail professionnel.</li>
+          <li>L'application doit être utilisée sur un téléphone professionnel.</li>
+          <li>Les ordinateurs ou téléphones personnels ne doivent pas être utilisés pour accéder au service.</li>
+        </ol>
+        <p>
+          Tout utilisateur s'engage à garantir la véracité des données renseignées. Les équipes doivent s'assurer d'utiliser des ordinateurs,
+          smartphones ou tablettes protégés par des mots de passe et des antivirus pour éviter tout risque de violation des données.
+        </p>
+        <h2 className="tw-mt-4 tw-mb-2 tw-text-main">Falsification et duplication des données</h2>
+        <ol className="tw-list-inside tw-list-disc">
+          <li>La duplication des données pour un autre usage que celui prévu dans le cadre strict de la mission est interdite.</li>
+          <li>
+            Tout manquement à ces engagements peut conduire MANO à retirer l'utilisation de l'outil à un professionnel, une équipe ou une
+            organisation.
+          </li>
+        </ol>
       </main>
       <ButtonCustom
         className="tw-m-auto tw-mt-20 tw-w-56 tw-rounded-3xl tw-text-base"
