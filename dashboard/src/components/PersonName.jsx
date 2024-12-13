6cd9708784816451762a888af2334c4ddd78567b
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-12-13 16:27:09 +0100

    chore: move get-person-infos for vite to hot refresh (#812)

--- dashboard/src/components/PersonName.jsx
+++ dashboard/src/components/PersonName.jsx
@@ -2,27 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { personsObjectSelector } from "../recoil/selectors";
-import { dayjsInstance, formatAge } from "../services/date";
-
-export function getPersonInfo(person) {
-  let infos = [];
-  if (!person) return "";
-  if (person.birthdate) {
-    infos.push(`Âge : ${formatAge(person.birthdate)} (${dayjsInstance(person.birthdate).format("DD/MM/YYYY")})`);
-  }
-  infos.push(`Genre : ${person.gender ?? ""}`);
-  infos.push(`Suivi·e depuis le : ${dayjsInstance(person.followedSince || person.createdAt).format("DD/MM/YYYY")}`);
-  if (person.wanderingAt) {
-    infos.push(`En rue depuis le : ${dayjsInstance(person.wanderingAt).format("DD/MM/YYYY")}`);
-  }
-  if (person.phone) {
-    infos.push(`Téléphone : ${person.phone}`);
-  }
-  if (person.email) {
-    infos.push(`Email : ${person.email}`);
-  }
-  return infos.join("\n");
-}
+import { getPersonInfo } from "../utils/get-person-infos";
 
 export default function PersonName({ item, onClick = null, redirectToTab = "Résumé" }) {
   const history = useHistory();
