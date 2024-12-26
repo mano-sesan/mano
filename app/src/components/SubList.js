8392418d087500942a81201200006d01c8fabd36
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-12-26 13:32:15 +0100

    fix(app): le nombre de documents indiqués dans le dossier médical sur mobile ne compte plus les dossiers (#846)

--- app/src/components/SubList.js
+++ app/src/components/SubList.js
@@ -6,7 +6,7 @@ import Button from './Button';
 
 const initNumberToShow = 10;
 
-const SubList = ({ label, onAdd, data, renderItem, ifEmpty, children, testID = 'list', disableVoirPlus = false }) => {
+const SubList = ({ label, onAdd, data, renderItem, ifEmpty, children, testID = 'list', disableVoirPlus = false, customCount = null }) => {
   const [expanded, setExpanded] = useState(false);
   const [numberToShow, setNumberToShow] = useState(initNumberToShow);
 
@@ -39,7 +39,7 @@ const SubList = ({ label, onAdd, data, renderItem, ifEmpty, children, testID = '
     <>
       <ListLabel testID={`${testID}-label`}>
         <ButtonExpand onPress={() => setExpanded(!expanded)} expanded={expanded} testID={`${testID}-expand`} />
-        <LabelStyled>{`${label}${data != null ? ` (${data.length})` : ''}`}</LabelStyled>
+        <LabelStyled>{`${label}${data != null ? ` (${customCount !== null ? customCount : data.length})` : ''}`}</LabelStyled>
         {Boolean(onAdd) && <Button caption="Ajouter" onPress={onAdd} noBorder testID={`${testID}-add`} />}
       </ListLabel>
       {!!children && expanded && <ChildrenContainer>{children}</ChildrenContainer>}
