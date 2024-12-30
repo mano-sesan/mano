6e6121bff022709c9dbadfa1b52e29f9477c8b12
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2024-12-30 14:59:22 +0100

    feat: age en jours si l'age est inférieur à 2 mois (web+mobile) (#866)

--- app/src/services/dateDayjs.js
+++ app/src/services/dateDayjs.js
@@ -42,6 +42,10 @@ export function formatAge(date) {
   if (!date) return null;
   const birthDate = dayjs(date);
   const months = Math.abs(dayjs().diff(birthDate, 'months'));
+  if (months < 2) {
+    const days = Math.abs(dayjs().diff(birthDate, 'days'));
+    return days + ' jour' + (days > 1 ? 's' : '');
+  }
   if (months < 24) return months + ' mois';
   const years = Math.abs(dayjs().diff(birthDate, 'years'));
   if (years < 7) {
