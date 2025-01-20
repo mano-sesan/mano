6bed76d98e7251252fc1dadffa9b89520b62a662
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2025-01-20 09:08:06 +0100

    chore: le mobile n'efface pas l'adresse IP si jamais il n'y a pas de wifi (#886)

--- app/get-ip.sh
+++ app/get-ip.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -e
-sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$(ipconfig getifaddr en0)/g" .env
+
+ip_address=$(ipconfig getifaddr en0 || true)
+
+if [[ $ip_address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Adresse IP valide : $ip_address"
+  sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$ip_address/g" .env
+else
+  echo "Erreur : l'adresse IP obtenue n'est pas valide ou est vide."
+fi
