1b76e87d2e6eecaa16bd8411c09f0874898487e8
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-03-17 15:38:11 +0100

    fix: quand on change la configuration des dossiers par défaut, les dossiers par défaut supprimés de la configuration et qui n'ont pas de document ne sont plus affichés, mais ceux qui ont des documents restent tels quels

--- dashboard/src/scenes/person/components/PersonDocuments.tsx
+++ dashboard/src/scenes/person/components/PersonDocuments.tsx
@@ -9,7 +9,7 @@ import { capture } from "../../../services/sentry";
 import { DocumentsModule } from "../../../components/DocumentsGeneric";
 import { groupsState } from "../../../recoil/groups";
 import type { PersonPopulated, PersonInstance } from "../../../types/person";
-import type { Document, FolderWithLinkedItem, LinkedItem } from "../../../types/document";
+import type { Document, DocumentWithLinkedItem, FolderWithLinkedItem, LinkedItem } from "../../../types/document";
 import type { UUIDV4 } from "../../../types/uuid";
 import { personsObjectSelector } from "../../../recoil/selectors";
 import { encryptAction } from "../../../recoil/actions";
@@ -47,7 +47,7 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
     createdBy: "admin",
   };
 
-  const defaultDocuments: Array<FolderWithLinkedItem> = (organisation.defaultPersonsFolders || []).map((folder) => ({
+  const defaultFolders: Array<FolderWithLinkedItem> = (organisation.defaultPersonsFolders || []).map((folder) => ({
     ...folder,
     movable: false,
     linkedItem: {
@@ -55,17 +55,21 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
       type: "person",
     } as LinkedItem,
   }));
-  const defaultDocumentsIds = defaultDocuments.map((d) => d._id);
 
+  const cleanedDocs = removeOldDefaultFolders(
+    // Les documents et dossiers de la personne...
+    [
+      ...(person.documentsForModule || []),
+      // Les documents et dossier du groupe (famille)
+      ...(person.groupDocuments || []),
+    ],
+    defaultFolders
+  );
   const documents = [
     // Le dossier "Actions" est ajouté si nécessaire, il s'affichera toujours en premier
     needsActionsFolder ? actionsFolder : undefined,
-    // Les documents et dossiers de la personne (auxquels on supprime les dossiers par défaut)
-    ...(person.documentsForModule || []).filter((d) => !defaultDocumentsIds.includes(d._id)),
-    // Les documents et dossier du groupe (famille)
-    ...(person.groupDocuments || []),
+    ...cleanedDocs,
     // Les dossiers par défaut configurés par l'organisation
-    ...defaultDocuments,
   ]
     .filter((e) => e)
     .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
@@ -158,7 +162,7 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
               ..._person,
               // If there are no document yet and default documents are present,
               // we save the default documents since they are modified by the user.
-              documents: (_person.documents || [...defaultDocuments])
+              documents: (_person.documents || [...defaultFolders])
                 .filter((f) => f._id !== folder._id)
                 .map((item) => {
                   if (item.parentId === folder._id) return { ...item, parentId: "" };
@@ -243,7 +247,7 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
                 ..._person,
                 // If there are no document yet and default documents are present,
                 // we save the default documents since they are modified by the user.
-                documents: (_person.documents || [...defaultDocuments])?.filter((d) => d._id !== document._id),
+                documents: (_person.documents || [...defaultFolders])?.filter((d) => d._id !== document._id),
               }),
             });
           });
@@ -292,7 +296,7 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
                 ..._person,
                 // If there are no document yet and default documents are present,
                 // we save the default documents since they are modified by the user.
-                documents: (_person.documents || [...defaultDocuments])?.map((d) => {
+                documents: (_person.documents || [...defaultFolders])?.map((d) => {
                   if (d._id === documentOrFolder._id) return documentOrFolder;
                   return d;
                 }),
@@ -309,7 +313,7 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
       }}
       onAddDocuments={async (newDocuments) => {
         const [personError] = await tryFetchExpectOk(async () => {
-          const oldDocuments = person.documents?.length ? [...person.documents] : [...defaultDocuments];
+          const oldDocuments = person.documents?.length ? [...person.documents] : [...defaultFolders];
           return API.put({
             path: `/person/${person._id}`,
             body: await encryptPerson({
@@ -333,3 +337,64 @@ const PersonDocuments = ({ person }: PersonDocumentsProps) => {
 };
 
 export default PersonDocuments;
+
+function removeOldDefaultFolders(docsOrFolders: Array<FolderWithLinkedItem | DocumentWithLinkedItem>, defaultFolders: Array<FolderWithLinkedItem>) {
+  // Scénario: une organisation paramètre des dossiers par défaut, le dossier "Dossier A" est créé
+  // et plus tard change ce paramétrage, et ne mets plus de "Dossier A" dans la configuration
+  // il reste donc des dossiers vides, auparavant configurés par l'organisation
+  // ce n'est pas pertinent pour l'utilisateur de voir ces dossiers vides, donc on les masque
+
+  const defaultFoldersIds = defaultFolders.map((d) => d._id);
+  const foldersFromPreviousDefaultFolders: Array<FolderWithLinkedItem> = [];
+  const validItems: Array<FolderWithLinkedItem | DocumentWithLinkedItem> = [...defaultFolders];
+  for (let item of docsOrFolders) {
+    // si ce n'est pas un dossier, c'est un document, on l'affiche
+    if (item.type !== "folder") {
+      validItems.push(item);
+      continue;
+    }
+    item = item as FolderWithLinkedItem;
+    // Seuls les dossiers avec `movable` à `false` sont des dossiers potentiellement par défault
+    // de la configuration actuelle ou ancienne des dossiers par défaut de l'organisation
+    if (defaultFoldersIds.includes(item._id)) {
+      // Si le dossier est dans la liste des dossiers par défaut,
+      // on passe au suivant
+      continue;
+    }
+    if (item.movable !== false) {
+      // Si le dossier n'a pas `movable === false`,
+      // c'est un dossier créé par l'utilisateur pour la personne, on l'affiche
+      validItems.push(item);
+      continue;
+    }
+    // Mous avons donc à faire avec un dossier par défaut de l'ancienne configuration
+    // qui n'est plus présent dans la configuration actuelle
+    // il faut voir s'il a des documents dedans
+    foldersFromPreviousDefaultFolders.push(item);
+  }
+
+  if (foldersFromPreviousDefaultFolders.length > 0) {
+    // on a des dossiers par défaut de l'ancienne configuration
+    // il faut voir s'ils ont des documents dedans
+    for (const item of foldersFromPreviousDefaultFolders) {
+      if (recursiveCheckIfFolderHasDocuments(item, docsOrFolders)) {
+        validItems.push(item);
+      }
+    }
+  }
+
+  return validItems;
+}
+
+function recursiveCheckIfFolderHasDocuments(folder: FolderWithLinkedItem, docsOrFolders: Array<FolderWithLinkedItem | DocumentWithLinkedItem>) {
+  const documents = docsOrFolders?.filter((d) => d.type === "document");
+  const folders = docsOrFolders?.filter((d) => d.type === "folder" && d._id !== folder._id);
+  for (const doc of documents) {
+    if (doc.parentId === folder._id) return true;
+  }
+  for (const folder of folders) {
+    if (folder.parentId !== folder._id) continue;
+    if (recursiveCheckIfFolderHasDocuments(folder as FolderWithLinkedItem, docsOrFolders)) return true;
+  }
+  return false;
+}
