0d60f0f174c3fe159203ad51ab76ab89c5975d45
Raphaël Huchet <raph@selego.co>
2025-09-15 09:56:25 +0200

    chore: fix migration gaveFeedbackSep2025 (#1075)

--- api/src/db/migrations/20250910080210-feedback-sep-2025.js
+++ api/src/db/migrations/20250910080210-feedback-sep-2025.js
@@ -5,14 +5,14 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(`
       ALTER TABLE "mano"."User"
-      ADD COLUMN "gaveFeedbackSep2025" BOOLEAN DEFAULT FALSE;
+      ADD COLUMN IF NOT EXISTS"gaveFeedbackSep2025" BOOLEAN DEFAULT FALSE;
     `);
   },
 
   async down(queryInterface) {
     await queryInterface.sequelize.query(`
       ALTER TABLE "mano"."User"
-      DROP COLUMN "gaveFeedbackSep2025";
+      DROP COLUMN IF EXISTS "gaveFeedbackSep2025";
     `);
   },
 };
