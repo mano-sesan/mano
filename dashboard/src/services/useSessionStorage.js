93711e2d691b25a51f4e9d82916ba7cf463eab13
Raphaël Huchet <raph@selego.co>
2025-12-11 12:53:26 +0100

    fix: remove useless files

--- dashboard/src/services/useSessionStorage.js
+++
@@ -1,8 +0,0 @@
-import { useSessionStorageValue } from "@react-hookz/web";
-
-export const useSessionStorage = (key, defaultValue) => {
-  const { value, set, remove } = useSessionStorageValue(key, {
-    defaultValue,
-  });
-  return [value, set, remove];
-};
