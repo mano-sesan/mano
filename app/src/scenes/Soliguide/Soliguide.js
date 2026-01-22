cb05137aff9d97ad4153c5226c6eee45018bb289
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2026-01-22 16:46:33 +0200

    clean: delete app folder

--- app/src/scenes/Soliguide/Soliguide.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WebView from "react-native-webview";
-
-const Soliguide = () => {
-  return <WebView source={{ uri: "https://soliguide.fr/" }} />;
-};
-
-export default Soliguide;
--- app/src/utils/sortByName.js
+++
@@ -1,1 +0,0 @@
-export const sortByName = (p1, p2) => p1?.name?.localeCompare(p2.name);
