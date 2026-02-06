4556d0fc4e3f34c07e8ca432f260bc1608802921
Raphaël Huchet <raphaelht@gmail.com>
2026-02-06 14:09:10 +0100

    Remove console log for medical file ID
    Removed console log for medical file ID.

--- dashboard/src/scenes/person/components/CommentsMedical.jsx
+++ dashboard/src/scenes/person/components/CommentsMedical.jsx
@@ -12,7 +12,6 @@ const CommentsMedical = ({ person }) => {
   const customFieldsMedicalFile = useAtomValue(customFieldsMedicalFileSelector);
 
   const medicalFile = person.medicalFile;
-  console.log("medical file id", medicalFile._id);
   const commentsMedical = useMemo(
     () => [...(person?.commentsMedical || [])].sort((a, b) => new Date(b.date || b.createdAt) - new Date(a.date || a.createdAt)),
     [person]
