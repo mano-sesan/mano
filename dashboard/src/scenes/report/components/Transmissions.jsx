a114ba421e3a961e0b7798bacf3f37a73bbdd86c
Pini <pini.tzr@gmail.com>
2026-02-17 13:18:13 +0100

    fix: improve spacing in ActionsCalendar night-session text and Comptes Rendus transmission cards

--- dashboard/src/scenes/report/components/Transmissions.jsx
+++ dashboard/src/scenes/report/components/Transmissions.jsx
@@ -39,7 +39,7 @@ export default function Transmissions({ userId, period, selectedTeamsObject, rep
           return (
             <details open={days.length === 1} className="tw-my-2 tw-p-2" key={day}>
               <summary>
-                <h4 className="tw-inline-block tw-text-base tw-capitalize">{dayjsInstance(day).format("dddd D MMM")}</h4>
+                <h4 className="tw-inline-block tw-text-base tw-capitalize tw-mb-2">{dayjsInstance(day).format("dddd D MMM")}</h4>
               </summary>
               {Object.entries(selectedTeamsObject).map(([teamId, team]) => {
                 const report = reports.find((report) => report.team === teamId && report.date === day);
@@ -159,7 +159,7 @@ function Transmission({ report, team, day, teamId, userId, reactSelectInputId, o
 
   return (
     <>
-      <div className="tw-p-4 tw-mb-4 tw-flex tw-flex-col tw-rounded-2xl tw-bg-gray-100">
+      <div className="tw-p-4 tw-mb-4 tw-flex tw-flex-col tw-gap-2 tw-rounded-2xl tw-bg-gray-100">
         <p className="tw-font-medium tw-flex tw-items-center tw-gap-1">
           {team?.nightSession ? <MoonIcon className="tw-h-5 tw-w-5 tw-text-blue-900" /> : <SunIcon className="tw-h-5 tw-w-5 tw-text-yellow-500" />}{" "}
           {team?.name || ""}
