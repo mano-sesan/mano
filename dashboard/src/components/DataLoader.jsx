27d3b4c4cc34fd1c65ba317943df35d34d67cf7e
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2026-03-06 12:07:31 +0200

    loading

--- dashboard/src/components/DataLoader.jsx
+++ dashboard/src/components/DataLoader.jsx
@@ -16,7 +16,7 @@ export default function DataLoader() {
 
   if (fullScreen) {
     return (
-      <div className="tw-absolute tw-inset-0 tw-z-[1000] tw-box-border tw-flex tw-w-full tw-items-center tw-justify-center tw-bg-white">
+      <div className="tw-absolute tw-inset-0 tw-z-[1000] tw-box-border tw-flex tw-w-full tw-items-center tw-justify-center tw-bg-[#E1E3E3]">
         <div className="tw-flex tw-h-[50vh] tw-max-h-[50vw] tw-w-[50vw] tw-max-w-[50vh] tw-flex-col tw-items-center tw-justify-center">
           <RandomPicture />
           <ProgressBar progress={progress} total={total} loadingText={loadingText} />
