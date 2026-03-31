f8bd001129f91d8d1857cea664f0289945436f21
Raphaël Huchet <raph@selego.co>
2026-03-31 15:43:53 +0200

    feat: meilleur affichage des dates en calendrier et navigation fixe (qui ne danse plus)

--- dashboard/src/components/ActionsCalendar.jsx
+++ dashboard/src/components/ActionsCalendar.jsx
@@ -12,13 +12,13 @@ import ExclamationMarkButton from "./tailwind/ExclamationMarkButton";
 import { CANCEL, DONE, sortActionsOrConsultations, TODO } from "../atoms/actions";
 import TagTeam from "./TagTeam";
 import { useLocalStorage } from "../services/useLocalStorage";
-import TabsNav from "./tailwind/TabsNav";
 import DescriptionIcon from "./DescriptionIcon";
 import ActionStatusSelect from "./ActionStatusSelect";
 import { defaultModalActionState, modalActionState } from "../atoms/modal";
 import DocumentIcon from "./DocumentIcon";
 import CommentIcon from "./CommentIcon";
 import { UserGroupIcon } from "@heroicons/react/16/solid";
+import { MoonIcon } from "@heroicons/react/24/solid";
 
 const ActionsCalendar = ({ actions, isNightSession, columns = ["Heure", "Nom", "Personne suivie", "Créée le", "Statut", "Équipe(s) en charge"] }) => {
   const setModalAction = useSetAtom(modalActionState);
@@ -205,40 +205,40 @@ const ActionsCalendar = ({ actions, isNightSession, columns = ["Heure", "Nom", "
     />
   );
 
-  const renderDate = (date) => {
-    const dateString = formatCalendarDate(date);
-    if (!isNightSession) return dateString;
-    switch (dateString) {
-      case "Aujourd'hui":
-        return "Ce soir";
-      case "Demain":
-        return "Demain soir";
-      case "Hier":
-        return "Hier soir";
-      default:
-        return `Nuit du ${dateString}`;
-    }
+  const renderDate = (date, count) => {
+    const dateString = formatCalendarDate(date, isNightSession);
+    return (
+      <div className="tw-flex tw-items-center tw-justify-center tw-gap-2">
+        {isNightSession && <MoonIcon className="tw-w-3 tw-h-3 tw-text-main" />}
+        <div className="tw-truncate">{dateString}</div>
+        <div className={`tw-text-xs tw-font-normal tw-rounded tw-text-white tw-px-1 ${count > 0 ? "tw-bg-main" : "tw-bg-gray-500"}`}>{count}</div>
+      </div>
+    );
   };
 
   return (
     <>
-      <TabsNav
-        className="tw-justify-center"
-        tabs={[
-          "<",
-          `${renderDate(subtractOneDay(currentDate))} (${theDayBeforeActions.length})`,
-          `${renderDate(currentDate)} (${theCurrentDayActions.length})`,
-          `${renderDate(addOneDay(currentDate))} (${theDayAfterActions.length})`,
-          ">",
-        ]}
-        onClick={(_tab, index) => {
-          if (index === 0) return setCurrentDate(subtractOneDay(currentDate));
-          if (index === 4) return setCurrentDate(addOneDay(currentDate));
-          setActiveTabIndex(index);
-        }}
-        activeTabIndex={activeTabIndex}
-      />
-
+      <nav className="noprint tw-flex tw-w-full tw-text-sm" aria-label="Tabs">
+        <ul
+          className={`!tw-p-0 tw-flex tw-w-full tw-overflow-x-auto tw-list-none tw-items-center tw-justify-center tw-gap-1 tw-border-b tw-border-main/10`}
+        >
+          <Tab isChevron onClick={() => setCurrentDate(subtractOneDay(currentDate))}>
+            &lt;
+          </Tab>
+          <Tab isActive={activeTabIndex === 0} onClick={() => setActiveTabIndex(0)}>
+            {renderDate(subtractOneDay(currentDate), theDayBeforeActions.length)}
+          </Tab>
+          <Tab isActive={activeTabIndex === 1} onClick={() => setActiveTabIndex(1)}>
+            {renderDate(currentDate, theCurrentDayActions.length)}
+          </Tab>
+          <Tab isActive={activeTabIndex === 2} onClick={() => setActiveTabIndex(2)}>
+            {renderDate(addOneDay(currentDate), theDayAfterActions.length)}
+          </Tab>
+          <Tab isChevron onClick={() => setCurrentDate(addOneDay(currentDate))}>
+            &gt;
+          </Tab>
+        </ul>
+      </nav>
       <div className="tw-mb-5">
         {!!isNightSession && (
           <p className="tw-m-0 tw-text-center tw-text-xs tw-text-gray-500">
@@ -247,13 +247,13 @@ const ActionsCalendar = ({ actions, isNightSession, columns = ["Heure", "Nom", "
         )}
       </div>
       <div>
-        {activeTabIndex === 1 && (
+        {activeTabIndex === 0 && (
           <div className={theDayBeforeActions.length ? "tw-pb-8" : ""}>{renderActionsTable(theDayBeforeActions, subtractOneDay(currentDate))}</div>
         )}
-        {activeTabIndex === 2 && (
+        {activeTabIndex === 1 && (
           <div className={theCurrentDayActions.length ? "tw-pb-8" : ""}>{renderActionsTable(theCurrentDayActions, currentDate)}</div>
         )}
-        {activeTabIndex === 3 && (
+        {activeTabIndex === 2 && (
           <div className={theDayAfterActions.length ? "tw-pb-8" : ""}>{renderActionsTable(theDayAfterActions, addOneDay(currentDate))}</div>
         )}
       </div>
@@ -261,4 +261,22 @@ const ActionsCalendar = ({ actions, isNightSession, columns = ["Heure", "Nom", "
   );
 };
 
+function Tab({ isActive = false, onClick, isChevron = false, children }) {
+  return (
+    <li>
+      <button
+        type="button"
+        onClick={onClick}
+        className={[
+          isActive ? "tw-bg-main/10 tw-text-black" : "hover:tw-bg-gray-100 tw-text-main",
+          "tw-truncate tw-border-x tw-border-t tw-rounded-t-md tw-border-main/10 tw-py-2 tw-text-sm tw-font-medium",
+          isChevron ? "tw-px-3" : "tw-w-40",
+        ].join(" ")}
+        aria-current={isActive ? "page" : undefined}
+      >
+        {children}
+      </button>
+    </li>
+  );
+}
 export default ActionsCalendar;
