85639dc0b62f2a12e47e6d86b87db3c23178248e
Raphaël Huchet <raphaelht@gmail.com>
2026-04-27 15:36:43 +0200

    feat: dans les nouvelles stats, le filtre est par défaut sur les personnes suivies et l'ordre des options est réorganisé (#1635)

--- dashboard/src/scenes/stats/GeneralStats.jsx
+++ dashboard/src/scenes/stats/GeneralStats.jsx
@@ -30,16 +30,6 @@ const GeneralStats = ({
         {isStatsV2 ? (
           <>
             <Block
-              data={personTypeCounts?.all}
-              title="Toutes les personnes"
-              help="Toutes les personnes assignées à au moins une des équipes sélectionnées pendant la période sélectionnée, qu'il y ait eu une interaction ou non."
-            />
-            <Block
-              data={personTypeCounts?.modified}
-              title="Personnes mises à jour"
-              help="Personnes assignées à au moins une des équipes sélectionnées pendant la période sélectionnée, pour lesquelles il y a eu au moins une interaction durant la période, quel que soit leur statut au moment de la modification, y compris pendant qu'elles sont en dehors de la file active ou en dehors des équipes sélectionnées."
-            />
-            <Block
               data={personTypeCounts?.followed}
               title="Personnes suivies"
               help="Personnes assignées à au moins une des équipes sélectionnées pendant la période sélectionnée, pour lesquelles il y a eu au moins une interaction durant la période, en ne comptant que les interactions réalisées lorsque la personne était présente dans une des équipes sélectionnées et non sortie de file active."
@@ -48,6 +38,16 @@ const GeneralStats = ({
               data={personTypeCounts?.created}
               title="Nouvelles personnes"
               help="Personnes qui ont rejoint une des équipes sélectionnées pour la première fois ou dont le suivi a commencé durant la période sélectionnée."
+            />
+            <Block
+              data={personTypeCounts?.modified}
+              title="Personnes mises à jour"
+              help="Personnes assignées à au moins une des équipes sélectionnées pendant la période sélectionnée, pour lesquelles il y a eu au moins une interaction durant la période, quel que soit leur statut au moment de la modification, y compris pendant qu'elles sont en dehors de la file active ou en dehors des équipes sélectionnées."
+            />
+            <Block
+              data={personTypeCounts?.all}
+              title="Toutes les personnes"
+              help="Toutes les personnes assignées à au moins une des équipes sélectionnées pendant la période sélectionnée, qu'il y ait eu une interaction ou non."
             />
           </>
         ) : (
