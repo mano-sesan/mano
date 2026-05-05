bbddaa46f1d84c6627adcef3b542b33bc7b4fe21
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2026-05-05 19:42:30 +0300

    feat: force v3.22.0

--- api/src/middleware/versionCheck.js
+++ api/src/middleware/versionCheck.js
@@ -3,7 +3,7 @@ const { sequelize } = require("../db/sequelize");
 const { MOBILE_APP_VERSION } = require("../config");
 const { getAppLinks } = require("../utils/appLinks");
 
-const MINIMUM_MOBILE_APP_VERSION = [3, 20, 0];
+const MINIMUM_MOBILE_APP_VERSION = [3, 22, 0];
 
 let deploymentCommit = null;
 let deploymentDate = null;
