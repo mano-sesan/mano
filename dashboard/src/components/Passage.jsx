05b83f27a37d722fb320705ce5e246373b3db363
Raphaël Huchet <raph@selego.co>
2026-05-15 17:26:22 +0200

    fix: up

--- dashboard/src/components/Passage.jsx
+++ dashboard/src/components/Passage.jsx
@@ -69,7 +69,9 @@ const Passage = ({ passage, personId, onFinished }) => {
           if (!body.date) return toast.error("La date est obligatoire");
           if (outOfBoundariesDate(body.date)) return toast.error("La date est hors limites (entre 1900 et 2100)");
           if (!body.team) return toast.error("L'équipe est obligatoire");
-          if (body.anonymous && !body.anonymousNumberOfPassages) return toast.error("Veuillez spécifier le nombre de passages anonymes");
+          if (body.anonymous && (!body.anonymousNumberOfPassages || Number(body.anonymousNumberOfPassages) < 1)) {
+            return toast.error("Veuillez spécifier un nombre de passages anonymes (au moins 1)");
+          }
           let noPerson = !body.persons?.length && !body.person?.length;
           if (!body.anonymous && noPerson && !isEditingAnonymous) {
             return toast.error("Veuillez spécifier une personne");
